612f3a4cf137b1de0b800d369cef920c2d83813f
michael-rutledge <mrutledge2808@gmail.com>
2020-05-15 19:37:07 -0700

    Initial commit.

---
+++ app.js
@@ -0,0 +1,16 @@
+// SERVER REQUIRE STATEMENTS
+const express = require('express');
+const app = express();
+const serv = require('http').Server(app);
+const sio = require('socket.io')(serv, {});
+
+// START SERVER
+//
+// This app will run on port 3000 using index.html as its homepage. Direct access to client
+// content is also supported.
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/client/index.html");
+});
+app.use('/client', express.static(__dirname + '/client'));
+serv.listen(3000);
+console.log('Server started.');
