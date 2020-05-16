d5cfec5993ec9d22920e7a7e130b4f9d525e8d1f
michael-rutledge <mrutledge2808@gmail.com>
2020-05-16 13:23:16 -0700

    Add rudimentary room creation

--- server/game/Room.js
+++ server/game/Room.js
@@ -1,4 +1,9 @@
 // Encapsulates a room of Millionaire With Friends.
 class Room {
-  
+
+  constructor(roomCode) {
+    this.roomCode = roomCode;
+  }
 }
+
+module.exports = Room;
--- server/game/RoomPool.js
+++ server/game/RoomPool.js
@@ -2,6 +2,8 @@ const Hasher = require(process.cwd() + '/server/string/Hasher.js');
 const Logger = require(process.cwd() + '/server/logging/Logger.js');
 const Room = require(process.cwd() + '/server/game/Room.js');
 
+const ROOM_CODE_LENGTH= 4;
+
 // Holds all active rooms of Millionaire With Friends.
 class RoomPool {
 
@@ -25,6 +27,31 @@ class RoomPool {
     });
   }
 
+  // Attempts to add a user identified by the given username and socket to the room identified by
+  // the given room code.
+  addUserToRoom(socket, username, roomCode) {
+    // TODO: implement this.
+  }
+
+  // Reserves a new room with a new, random, and unused room code.
+  //
+  // Returns the new room code.
+  reserveNewRoom() {
+    var newRoomCode;
+
+    do {
+      newRoomCode = Hasher.genHash(ROOM_CODE_LENGTH);
+    } while(this.roomExists(newRoomCode));
+
+    this.rooms[newRoomCode] = new Room(newRoomCode);
+    return newRoomCode;
+  }
+
+  // Returns whether a room exists for the given room code.
+  roomExists(roomCode) {
+    return this.rooms.hasOwnProperty(roomCode);
+  }
+
   // Executes desired actions upon finished disconnect of the given socket.
   onDisconnect(socket) {
     Logger.logInfo('socket ' + socket.id + ' disconnected...');
@@ -43,6 +70,11 @@ class RoomPool {
   //  }
   userAttemptCreateRoom(socket, data) {
     Logger.logInfo('userAttemptCreateRoom');
+    // TODO: report failure back to user
+    if (!data || data.username.length < 1) { return; }
+    var newRoomCode = this.reserveNewRoom();
+    Logger.logInfo('new room code generated: ' + newRoomCode);
+    this.addUserToRoom(socket, data.username, newRoomCode);
   }
 
   // Attempts to join a user to a room using the given socket and data.
