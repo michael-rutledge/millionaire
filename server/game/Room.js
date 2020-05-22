f380a47d5fe238c0ca2371da80ed179ebda81f0d
michael-rutledge <mrutledge2808@gmail.com>
2020-05-22 10:24:27 -0700

    Add host logic to Room

--- server/game/Room.js
+++ server/game/Room.js
@@ -1,3 +1,4 @@
+const GameServer = require(process.cwd() + '/server/game/GameServer.js');
 const Player = require(process.cwd() + '/server/game/Player.js');
 
 // Encapsulates a room of Millionaire With Friends.
@@ -8,6 +9,9 @@ class Room {
     this.roomCode = roomCode;         // Room code (e.g. 'abcd' or 'foo1')
     this.usernameToPlayerMap = {};    // Map of usernames to Players
     this.socketIdToUsernameMap = {};  // Map of socket ids to usernames
+    this.host = undefined;            // Reference to host player
+    this.gameServer =                 // Game server that will handle game interactions
+        new GameServer(this.usernameToPlayerMap, this.socketIdToUsernameMap);
   }
 
 
@@ -30,9 +34,17 @@ class Room {
   //
   // Returns true if successful, false if unsuccessful.
   addPlayer(socket, username) {
-    // TODO: deal with games that are underway (i.e. allow people reconnecting)
-    if (!this._usernameExistsInRoom(username)) {
-      this.usernameToPlayerMap[username] = new Player(socket, username);
+    if (!this._usernameExistsInRoom(username) && !this.gameServer.isInGame()) {
+      var player = new Player(socket, username);
+      this.usernameToPlayerMap[username] = player;
+      this.socketIdToUsernameMap[socket.id] = username;
+      if (this.host === undefined) {
+        this.host = player;
+      }
+      return true;
+    } else if (this._usernameExistsInRoom(username) && this.gameServer.isInGame() &&
+               this.getPlayerByUsername(username).socket === undefined) {
+      this.getPlayerByUsername(username).socket = socket;
       this.socketIdToUsernameMap[socket.id] = username;
       return true;
     }
@@ -46,13 +58,22 @@ class Room {
   // while the game is running should merely disconnect the socket, making the player appear
   // offline.
   disconnectPlayer(socket) {
-    // TODO: deal with games that are underway
     var username = this.getUsernameBySocket(socket);
 
     if (this._socketExistsInRoom(socket)) {
-      delete this.socketIdToUsernameMap[socket.id];
+      var player = this.getPlayerBySocket(socket);
+      // Host leaving means a new host is needed.
+      if (this.socketIsHost(socket)) {
+        this.host = undefined;
+        delete this.socketIdToUsernameMap[socket.id];
+        this.reassignHost();
+        player.socket = undefined;
+      } else {
+        delete this.socketIdToUsernameMap[socket.id];
+        player.socket = undefined;
+      }
     }
-    if (this._usernameExistsInRoom(username)) {
+    if (this._usernameExistsInRoom(username) && !this.gameServer.isInGame()) {
       delete this.usernameToPlayerMap[username];
     }
   }
@@ -72,9 +93,23 @@ class Room {
     return this.usernameToPlayerMap[username];
   }
 
+  // Assigns the role of room host to a new socket if possible.
+  reassignHost() {
+    this.host = undefined;
+    if (!this.socketsEmpty()) {
+      this.host = this.usernameToPlayerMap[
+          this.socketIdToUsernameMap[Object.keys(this.socketIdToUsernameMap)[0]]];
+    }
+  }
+
   // Returns whether the amount of sockets connected is empty.
   socketsEmpty() {
     return Object.keys(this.socketIdToUsernameMap).length < 1;
+  }
+
+  // Returns whether the given socket is the socket of the Room's host.
+  socketIsHost(socket) {
+    return this._socketExistsInRoom(socket) && this.getPlayerBySocket(socket) == this.host;
   }
 }
 
--- test/server/game/RoomTest.js
+++ test/server/game/RoomTest.js
@@ -4,21 +4,42 @@ const MockSocket = require(process.cwd() + '/server/socket/MockSocket.js');
 const Player = require(process.cwd() + '/server/game/Player.js');
 const Room = require(process.cwd() + '/server/game/Room.js');
 
+// Mocks whether the given room is in game;
+function setRoomInGame(room, isInGame) {
+  room.gameServer.isInGame = () => { return isInGame };
+}
+
 describe('RoomTest', () => {
-  it('addPlayerShouldAddPlayerForNewUsername', () => {
+  it('addPlayerShouldAddPlayerForNewUsernameInLobby', () => {
     var room = new Room('test');
     var mockSocket = new MockSocket('socket_id');
+    setRoomInGame(room, false);
+    var expectedPlayer = new Player(mockSocket, 'username');
 
     var result = room.addPlayer(mockSocket, 'username');
 
     expect(result).to.be.true;
-    expect(room.getPlayerBySocket(mockSocket)).to.deep.equal(new Player(mockSocket, 'username'));
+    expect(room.getPlayerBySocket(mockSocket)).to.deep.equal(expectedPlayer);
   });
 
-  it('addPlayerShouldNotAddPlayerForExistingUsername', () => {
+  it('addPlayerShouldSetHostForFirstUsernameInLobby', () => {
     var room = new Room('test');
     var mockSocket1 = new MockSocket('socket_id_1');
     var mockSocket2 = new MockSocket('socket_id_2');
+    setRoomInGame(room, false);
+    var expectedHost = new Player(mockSocket1, 'host');
+
+    room.addPlayer(mockSocket1, 'host');
+    room.addPlayer(mockSocket2, 'joiner');
+
+    expect(room.host).to.deep.equal(expectedHost);
+  });
+
+  it('addPlayerShouldNotAddPlayerForExistingUsernameInLobby', () => {
+    var room = new Room('test');
+    var mockSocket1 = new MockSocket('socket_id_1');
+    var mockSocket2 = new MockSocket('socket_id_2');
+    setRoomInGame(room, false);
 
     room.addPlayer(mockSocket1, 'username');
     var result = room.addPlayer(mockSocket2, 'username');
@@ -27,15 +48,82 @@ describe('RoomTest', () => {
     expect(room.getPlayerBySocket(mockSocket2)).to.be.undefined;
   });
 
-  it('disconnectPlayerShouldRemovePlayer', () => {
+  it('addPlayerShouldAddPlayerForExistingUsernameWithoutSocketInGame', () => {
     var room = new Room('test');
     var mockSocket = new MockSocket('socket_id');
-
+    setRoomInGame(room, false);
     room.addPlayer(mockSocket, 'username');
+
+    setRoomInGame(room, true);
+    room.disconnectPlayer(mockSocket);
+    var result = room.addPlayer(mockSocket, 'username');
+
+    expect(result).to.be.true;
+    expect(room.getPlayerBySocket(mockSocket)).to.deep.equal(new Player(mockSocket, 'username'));
+  });
+
+  it('addPlayerShouldNotAddPlayerForExistingUsernameWithSocketInGame', () => {
+    var room = new Room('test');
+    var mockSocket = new MockSocket('socket_id');
+    var mockSocket2 = new MockSocket('socket_id_2');
+    setRoomInGame(room, false);
+    room.addPlayer(mockSocket, 'username');
+
+    setRoomInGame(room, true);
+    var result = room.addPlayer(mockSocket2, 'username');
+
+    expect(result).to.be.false;
+    expect(room.getPlayerBySocket(mockSocket2)).to.be.undefined;
+  });
+
+  it('addPlayerShouldNotAddPlayerForNonExistentUsernameInGame', () => {
+    var room = new Room('test');
+    var mockSocket = new MockSocket('socket_id');
+    setRoomInGame(room, true);
+
+    var result = room.addPlayer(mockSocket, 'username');
+
+    expect(result).to.be.false;
+    expect(room.getPlayerBySocket(mockSocket)).to.be.undefined;
+  });
+
+  it('disconnectPlayerShouldRemoveEntirePlayerInLobby', () => {
+    var room = new Room('test');
+    var mockSocket = new MockSocket('socket_id');
+    setRoomInGame(room, false);
+    room.addPlayer(mockSocket, 'username');
+
     room.disconnectPlayer(mockSocket);
 
     expect(room.getPlayerBySocket(mockSocket)).to.be.undefined;
     expect(room.getPlayerByUsername('username')).to.be.undefined;
+  });
+
+  it('disconnectPlayerShouldRemoveSocketButKeepPlayerByUsernameInGame', () => {
+    var room = new Room('test');
+    var mockSocket = new MockSocket('socket_id');
+    setRoomInGame(room, false);
+    room.addPlayer(mockSocket, 'username');
+
+    setRoomInGame(room, true);
+    room.disconnectPlayer(mockSocket);
+
+    expect(room.getPlayerBySocket(mockSocket)).to.be.undefined;
+    expect(room.getPlayerByUsername('username')).to.deep.equal(new Player(undefined, 'username'));
+  });
+
+  it('disconnectPlayerShouldSetNewHostIfHostDisconnects', () => {
+    var room = new Room('test');
+    var mockSocket = new MockSocket('socket_id');
+    var mockSocket2 = new MockSocket('socket_id_2');
+    setRoomInGame(room, false);
+    room.addPlayer(mockSocket, 'firstHost');
+    room.addPlayer(mockSocket2, 'newHost');
+
+    room.disconnectPlayer(mockSocket);
+
+    expect(room.host).to.not.deep.equal(new Player(mockSocket, 'firstHost'));
+    expect(room.host).to.deep.equal(new Player(mockSocket2, 'newHost'));
   });
 
   it('socketsEmptyShouldGiveExpectedResult', () => {
