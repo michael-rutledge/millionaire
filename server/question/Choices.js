2ecdf57c91b736fb521ee37a4acdb515932f4c5d
michael-rutledge <mrutledge2808@gmail.com>
2020-05-28 17:33:47 -0700

    Add MAX_CHOICES field to Choices.js

--- server/question/Choices.js
+++ server/question/Choices.js
@@ -2,8 +2,10 @@ const A = 0;
 const B = 1;
 const C = 2;
 const D = 3;
+const MAX_CHOICES = 4;
 
 module.exports.A = A;
 module.exports.B = B;
 module.exports.C = C;
 module.exports.D = D;
+module.exports.MAX_CHOICES = MAX_CHOICES;
