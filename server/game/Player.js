9ef0fa9cf4728a7894af0aad6ab2d245d0f96a86
michael-rutledge <mrutledge2808@gmail.com>
2020-05-28 17:33:09 -0700

    Add game-related fields to Player

--- server/game/Player.js
+++ server/game/Player.js
@@ -1,3 +1,5 @@
+const Choices = require(process.cwd() + '/server/question/Choices.js');
+
 // Encapsulates a player and their possible actions in Millionaire With Friends.
 class Player {
 
@@ -5,6 +7,52 @@ class Player {
   constructor(socket, username) {
     this.socket = socket;
     this.username = username;
+    this.money = 0;
+    this.fastestFingerChoices = [];
+    this.fastestFingerTime = undefined;
+    this.hotSeatChoice = undefined;
+    this.hotSeatTime = undefined;
+  }
+
+
+  // PUBLIC METHODS
+
+  // Adds the given choice to the Player's fastest finger choices.
+  //
+  // The choice is expected to be a value from Choices.js.
+  chooseFastestFinger(choice) {
+    if (!this.fastestFingerChoices.includes(choice) &&
+        this.fastestFingerChoices.length < Choices.MAX_CHOICES) {
+      this.fastestFingerChoices.push(choice);
+      // Last choice should keep track of the time to allow for calculating elapsed time
+      if (this.fastestFingerChoices.length == Choices.MAX_CHOICES) {
+        this.fastestFingerTime = new Date().getTime();
+      }
+    }
+  }
+
+  // Chooses an answer for a hot seat question for this Player.
+  //
+  // Time of answer is tracked in calculation of contestant money.
+  chooseHotSeat(choice) {
+    if (this.hotSeatChoice === undefined) {
+      this.hotSeatChoice = choice;
+      this.hotSeatTime = new Date().getTime();
+    }
+  }
+
+  // Clears all answers given by the Player.
+  clearAllAnswers() {
+    this.fastestFingerChoices = [];
+    this.fastestFingerTime = undefined;
+    this.hotSeatChoice = undefined;
+    this.hotSeatTime = undefined;
+  }
+
+  // Resets the player to base stats.
+  reset() {
+    this.money = 0;
+    this.clearAllAnswers();
   }
 }
 
---
+++ test/server/game/PlayerTest.js
@@ -0,0 +1,87 @@
+const expect = require('chai').expect;
+
+const Choices = require(process.cwd() + '/server/question/Choices.js');
+const MockSocket = require(process.cwd() + '/server/socket/MockSocket.js');
+const Player = require(process.cwd() + '/server/game/Player.js');
+
+describe('PlayerTest', () => {
+  it('chooseFastestFingerShouldAddChoiceWhenAvailable', () => {
+    var player = new Player(new MockSocket('socket_id'), 'username');
+
+    player.chooseFastestFinger(Choices.A);
+
+    expect(player.fastestFingerChoices).to.deep.equal([Choices.A]);
+  });
+
+  it('chooseFastestFingerShouldNotAddChoiceWhenAtCapacity', () => {
+    var player = new Player(new MockSocket('socket_id'), 'username');
+
+    player.chooseFastestFinger(Choices.A);
+    player.chooseFastestFinger(Choices.B);
+    player.chooseFastestFinger(Choices.C);
+    player.chooseFastestFinger(Choices.D);
+    player.chooseFastestFinger(Choices.A);
+
+    expect(player.fastestFingerChoices).to.deep.equal([Choices.A, Choices.B, Choices.C, Choices.D]);
+  });
+
+  it('chooseFastestFingerShouldNotAddChoiceWhenAlreadyPresent', () => {
+    var player = new Player(new MockSocket('socket_id'), 'username');
+
+    player.chooseFastestFinger(Choices.A);
+    player.chooseFastestFinger(Choices.A);
+
+    expect(player.fastestFingerChoices).to.deep.equal([Choices.A]);
+  });
+
+  it('chooseFastestFingerShouldTrackTimeOfAnswerWhenAllChoicesIn', () => {
+    var player = new Player(new MockSocket('socket_id'), 'username');
+    var timeBefore, timeAfter;
+
+    player.chooseFastestFinger(Choices.A);
+    player.chooseFastestFinger(Choices.B);
+    player.chooseFastestFinger(Choices.C);
+    timeBefore = player.fastestFingerTime;
+    player.chooseFastestFinger(Choices.D);
+    timeAfter = player.fastestFingerTime;
+
+    expect(timeBefore).to.be.undefined;
+    expect(timeAfter).to.not.be.undefined;
+  });
+
+  it('chooseHotSeatShouldChooseWhenNoChoiceMadeYet', () => {
+    var player = new Player(new MockSocket('socket_id'), 'username');
+
+    player.chooseHotSeat(Choices.A);
+
+    expect(player.hotSeatChoice).to.equal(Choices.A);
+    expect(player.hotSeatTime).to.not.be.undefined;
+  });
+
+  it('chooseHotSeatShouldNotChooseWhenChoiceAlreadyMade', () => {
+    var player = new Player(new MockSocket('socket_id'), 'username');
+    var timeBefore, timeAfter;
+
+    player.chooseHotSeat(Choices.A);
+    timeBefore = player.hotSeatTime;
+    player.chooseHotSeat(Choices.B);
+    timeAfter = player.hotSeatTime;
+
+    expect(player.hotSeatChoice).to.equal(Choices.A);
+    expect(timeBefore).to.not.be.undefined;
+    expect(timeBefore).to.equal(timeAfter);
+  });
+
+  it('clearAllAnswersShouldGiveExpectedResult', () => {
+    var player = new Player(new MockSocket('socket_id'), 'username');
+    player.chooseFastestFinger(Choices.A);
+    player.chooseHotSeat(Choices.A);
+
+    player.clearAllAnswers();
+
+    expect(player.fastestFingerChoices).to.be.empty;
+    expect(player.fastestFingerTime).to.be.undefined;
+    expect(player.hotSeatChoice).to.be.undefined;
+    expect(player.hotSeatTime).to.be.undefined;
+  });
+});
