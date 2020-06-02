52f4591bfeb691227db78879f40875657d72e58c
michael-rutledge <mrutledge2808@gmail.com>
2020-06-02 00:33:13 -0700

    Update Colors.js and Constants.js to include new values

--- client/js/rendering/Colors.js
+++ client/js/rendering/Colors.js
@@ -1,7 +1,11 @@
+const DEFAULT_TEXT_COLOR = '#ffffff';
+
 module.exports.BACKGROUND_VOID = '#150023';
 module.exports.BACKGROUND_SIDE_GRADIENT = '#00134d';
-module.exports.SIDE_PANEL_LINE_BASE = '#b1a3dc';
-module.exports.SIDE_PANEL_LINE_SHINE = '#ffffff';
+module.exports.DEFAULT_TEXT_COLOR = DEFAULT_TEXT_COLOR;
 module.exports.QUESTION_AND_CHOICES_FILL = '#000000';
 module.exports.QUESTION_AND_CHOICES_LINE_BASE = '#9cbfe2';
 module.exports.QUESTION_AND_CHOICES_LINE_SHINE = '#ffffff';
+module.exports.QUESTION_TEXT_COLOR = DEFAULT_TEXT_COLOR;
+module.exports.SIDE_PANEL_LINE_BASE = '#b1a3dc';
+module.exports.SIDE_PANEL_LINE_SHINE = '#ffffff';
