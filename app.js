7ec9ecc8a131dbe13b2523f587f8ea0add66e1d4
simbaa345 <82680186+simbaa345@users.noreply.github.com>
2021-10-19 21:26:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + "/client/index.html");
 });
 app.use('/client', express.static(__dirname + '/client'));
-serv.listen(3000);
+serv.listen(process.env.PORT || 3000);
 Logger.logInfo('Server started.');
 
 var roomPool = new RoomPool(sio);
